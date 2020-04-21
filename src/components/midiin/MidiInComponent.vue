e4857da6a3d0fb9d8ff32496b6c11dd36068d543
osx user <gili.shemesh@gmail.com>
2020-04-21 21:15:36 +0300

    commit

--- src/components/midiin/MidiInComponent.vue
+++ src/components/midiin/MidiInComponent.vue
@@ -1,140 +1,149 @@
 <template>
-  <div>
-    <header class="w3-container w3-teal">
-      <h1>{{ headrMsg }}</h1>
-    </header>
+  <Page :text="headrMsg"> 
+      <Card
+        :mt="true"
+        v-for="(item, index) in midiRouteInput.midiRouterChains"
+        v-bind:key="index"
+      >
+        <MidiFilterChain v-bind:chainItem="item" v-bind:chainIdx="index" v-bind:midiId="midiId" v-on:do-splice="spliceRouterChain($event,1)"></MidiFilterChain>
+      </Card>
 
-    <div class="w3-container "  >  
-
-      
-
-      <div class="w3-card-4 w3-margin-top" v-for="(item, index) in midiRouteInput.midiRouterChains" v-bind:key="index" >
-        <MidiFilterChain  v-bind:chainItem ="item" v-bind:chainIdx="index" v-bind:midiId="midiId" 
-              ></MidiFilterChain>
-
-
-      </div>
-
-
-    
-
-
-
-    <div class="w3-card-4">
+      <Card :mt="true">
         <p>
-          <button
-              class="w3-button w3-section w3-teal w3-ripple w3-margin-left"
-              @click="addChain()"
-            >Add chain</button>
+          <Btn
+            :ml="true"
+            @click="addChain()"
+          >Add chain</Btn>
         </p>
-      </div>
+      </Card>
 
-
-      <div class="w3-container w3-card-4">
+      <Card :mt="true">
+        <CardBody>
         <h5>Ignore Types</h5>
         <p>
-          <input class="w3-check" type="checkbox"  
-            v-model="midiRouteInput.ignoreTypes.midiSysex"/>
+          <input class="w3-check" type="checkbox" v-model="midiRouteInput.ignoreTypes.midiSysex" />
           <label>Sysex</label>
-          <input class="w3-check" type="checkbox" 
-            v-model="midiRouteInput.ignoreTypes.midiTime"/>
+          <input class="w3-check" type="checkbox" v-model="midiRouteInput.ignoreTypes.midiTime" />
           <label>Time</label>
-          <input class="w3-check" type="checkbox"  
-            v-model="midiRouteInput.ignoreTypes.midiSense"/>
+          <input class="w3-check" type="checkbox" v-model="midiRouteInput.ignoreTypes.midiSense" />
           <label>Sense</label>
         </p>
-       <h5>Clock</h5>
-        <div>
-        <div class="w3-container  w3-cell">
-          <label>Time signature from SPP</label>
-          <input class="w3-input" type="number"  v-model.number="midiRouteInput.midiRouteClock.fromSppPos">
-        </div> 
-        </div>
-      <div>
-        <div class="w3-container  w3-cell">
-          <label>upper numeral</label>
-          <input class="w3-input" type="number" v-model.number="midiRouteInput.midiRouteClock.timeSig">
-        </div>
-        <div class="w3-container  w3-cell">
-          <label>lower numeral</label>
-          <input class="w3-input" type="number" v-model.number="midiRouteInput.midiRouteClock.timeSigDivBy">
-        </div> 
-     
-    </div>
-      <div>
-        <div class="w3-container  w3-cell w3-cell-middle">
-        Propagate Inputs 
-        </div>
-        <div class="w3-container  w3-cell w3-cell-middle">
-          <select class="w3-select" name="option"  v-model="propegateInputToAdd">
-            <option value=""></option>
-            <option v-for="(item) in inPortsWithoutSelfClockPropegate" v-bind:key="item.midiInputId" v-bind:value="item.midiInputId"  >{{item.midiInputId}} - {{item.midiInputName}}</option>
-          </select>
-        </div>
-        <div class="w3-container  w3-cell w3-cell-middle">
-           <button
-            class="w3-button w3-section w3-teal w3-ripple w3-margin-left "
-            @click="addClockPropegateInput"
-          >+</button> 
-        </div>
-      </div>
+        <h5>Clock</h5>
+        <Row>
+          <RowCell>
+            <label>Time signature from SPP</label>
+            <input
+              class="w3-input"
+              type="number"
+              v-model.number="midiRouteInput.midiRouteClock.fromSppPos"
+            />
+          </RowCell>
+        </Row>
+        <Row>
+          <RowCell>
+            <label>upper numeral</label>
+            <input
+              class="w3-input"
+              type="number"
+              v-model.number="midiRouteInput.midiRouteClock.timeSig"
+            />
+          </RowCell>
+          <RowCell>
+            <label>lower numeral</label>
+            <input
+              class="w3-input"
+              type="number"
+              v-model.number="midiRouteInput.midiRouteClock.timeSigDivBy"
+            />
+          </RowCell>
+        </Row>
+        <Row>
+          <RowCell>Propagate Inputs</RowCell>
+          <RowCell>
+            <select class="w3-select" name="option" v-model="propegateInputToAdd">
+              <option value></option>
+              <option
+                v-for="(item) in inPortsWithoutSelfClockPropegate"
+                v-bind:key="item.midiInputId"
+                v-bind:value="item.midiInputId"
+              >{{item.midiInputId}} - {{item.midiInputName}}</option>
+            </select>
+          </RowCell>
+          <RowCell>
+            <Btn
+              :ml="true"
+              @click="addClockPropegateInput()"
+            >+</Btn>
+          </RowCell>
+        </Row>
 
-<p>
-      <ul class="w3-ul w3-border w3-hoverable w3-margin-left">
-<li class="w3-display-container" v-for="(item) in midiRouteInput.midiRouteClock.propegateInputs" 
-    v-bind:key="item.midiInputId"  > {{item.midiInputId}} - {{item.midiInputName}}
-  <span @click="spliceClockPropegateInput(item.midiInputId)" class="w3-button w3-transparent w3-display-right">&times;</span></li>
-      </ul>
-  </p>
+        <Row>
+          <ul class="w3-ul w3-border w3-hoverable w3-margin-left">
+            <li
+              class="w3-display-container"
+              v-for="(item) in midiRouteInput.midiRouteClock.propegateInputs"
+              v-bind:key="item.midiInputId"
+            >
+           
+              {{item.midiInputId}} - {{item.midiInputName}}
+            
+              <RowCellRight>
+                <BtnHref @click="spliceClockPropegateInput(item.midiInputId)">Delete</BtnHref>
+              </RowCellRight>
+            </li>
+          </ul>
+        </Row>
 
-              <h5>14 Bit CC </h5>
-    
+        <h5>14 Bit CC</h5>
+        <Row>
+          <RowCell>
+            <label>Channel</label>
+            <input
+              class="w3-input"
+              type="number"
+              v-model.number="cc14bitChannel"
+              @focus="$event.target.select()"
+            />
+          </RowCell>
+          <RowCell>
+            <label>CC</label>
+            <input
+              class="w3-input"
+              type="number"
+              v-model.number="cc14bitCc"
+              @focus="$event.target.select()"
+            />
+          </RowCell>
 
-       <div>
+          <RowCell>
+            <Btn
+              :ml="true"
+              @click="addCc14bit()"
+            >+</Btn>
+          </RowCell>
+        </Row>
 
-
-            <div class="w3-container  w3-cell">
-          <label>Channel</label>
-          <input class="w3-input" type="number" v-model.number="cc14bitChannel" @focus="$event.target.select()">
-            </div>
-
-        <div class="w3-container w3-cell">
-          <label>CC</label>
-          <input class="w3-input" type="number" v-model.number="cc14bitCc" @focus="$event.target.select()">
-            </div>
-
-        <div class="w3-container w3-cell">
-           <button
-            class="w3-button w3-section w3-teal w3-ripple w3-margin-left "
-            @click="addCc14bit"
-          >+</button> 
-
-            </div>            
-       </div>
-       
-       
-        <p>
-          
-      <ul class="w3-ul w3-border w3-hoverable w3-margin-left">
-<li class="w3-display-container" v-for="(row, idx) in midiRouteInput.cc14bitAry" v-bind:key="idx" >Channel {{row.channel}} CC {{row.cc}} 
-  <span @click="spliceCc14bit(idx)" class="w3-button w3-transparent w3-display-right">&times;</span></li>
-
-      </ul>
-
-
- 
-
-  </p>
-
-      </div>
-  <p>
-    &nbsp;
-    </p>
-
-    </div>
+        <Row>
+          <ul class="w3-ul w3-border w3-hoverable w3-margin-left">
+            <li
+              class="w3-display-container"
+              v-for="(row, idx) in midiRouteInput.cc14bitAry"
+              v-bind:key="idx"
+            >
+              Channel {{row.channel}} CC {{row.cc}}
 
 
-  </div>
+              <RowCellRight>
+                <BtnHref @click="spliceCc14bit(idx)">Delete</BtnHref>
+              </RowCellRight>
+            </li>
+          </ul>
+        </Row>
+        <p>&nbsp;</p>
+     </CardBody>
+      </Card>
+      <p>&nbsp;</p>
+    </page>
 </template>
 
 <script lang="ts">
@@ -143,9 +152,18 @@ import { mapState } from "vuex";
 import { mapGetters } from "vuex";
 
 import * as Connection from "../../src/connection";
-import {LoginStatus} from '../../src/loginStatus'
+import { LoginStatus } from "../../src/loginStatus";
 
-import MidiFilterChain from "./MidiInFilterChainComponent.vue"
+import MidiFilterChain from "./MidiInFilterChainComponent.vue";
+
+import Page from "../a/Page.vue";
+import Card from "../a/Card.vue";
+import CardBody from "../a/CardBody.vue";
+import Row from "../a/Row.vue";
+import RowCell from "../a/RowCell.vue";
+import BtnHref from "../a/BtnHref.vue";
+import RowCellRight from "../a/RowCellRight.vue";
+import Btn from "../a/Btn.vue";
 
 @Component({
   computed: {
@@ -153,8 +171,16 @@ import MidiFilterChain from "./MidiInFilterChainComponent.vue"
     ...mapGetters(["isLoggedIn"])
   },
   components: {
-    MidiFilterChain
-    }
+    MidiFilterChain,
+    Page,
+    Card,
+    CardBody,
+    Row,
+    RowCell,
+    RowCellRight,
+    Btn,
+    BtnHref
+  }
 })
 export default class MidiInComponent extends Vue {
   @Prop() private midiId!: string;
@@ -163,59 +189,88 @@ export default class MidiInComponent extends Vue {
 
   loginStatus!: LoginStatus;
 
-  cc14bitChannel = 1
-  cc14bitCc = 0
+  cc14bitChannel = 1;
+  cc14bitCc = 0;
 
-  spliceCc14bit(idx: number){
-    this.midiRouteInput.cc14bitAry.splice(idx,1);
+  spliceRouterChain(idx: number){
+    this.midiRouteInput.midiRouterChains.splice(idx,1)
     this.$forceUpdate();
   }
 
-  addCc14bit(){
-    if ( this.cc14bitChannel< 1 || this.cc14bitChannel> 16 || this.cc14bitCc < 0 || this.cc14bitCc > 17) {
+  spliceCc14bit(idx: number) {
+    this.midiRouteInput.cc14bitAry.splice(idx, 1);
+    this.$forceUpdate();
+  }
+
+  addCc14bit() {
+    if (
+      this.cc14bitChannel < 1 ||
+      this.cc14bitChannel > 16 ||
+      this.cc14bitCc < 0 ||
+      this.cc14bitCc > 17
+    ) {
       return;
-      }
-    if ( this.midiRouteInput.cc14bitAry.filter( (row)=>{return row.channel === this.cc14bitChannel && row.cc === this.cc14bitCc}).length > 0 ) {
+    }
+    if (
+      this.midiRouteInput.cc14bitAry.filter(row => {
+        return row.channel === this.cc14bitChannel && row.cc === this.cc14bitCc;
+      }).length > 0
+    ) {
       return;
     }
     this.midiRouteInput.addCc14bitAry(this.cc14bitChannel, this.cc14bitCc);
-    
+
     this.$forceUpdate();
   }
 
-  spliceClockPropegateInput(inputsId: number){
-    for (let i=0;i<this.midiRouteInput.midiRouteClock.propegateInputs.length;i++){
-      if(this.midiRouteInput.midiRouteClock.propegateInputs[i].midiInputId === inputsId ){
-        this.midiRouteInput.midiRouteClock.propegateInputs.splice(i,1);
+  spliceClockPropegateInput(inputsId: number) {
+    for (
+      let i = 0;
+      i < this.midiRouteInput.midiRouteClock.propegateInputs.length;
+      i++
+    ) {
+      if (
+        this.midiRouteInput.midiRouteClock.propegateInputs[i].midiInputId ===
+        inputsId
+      ) {
+        this.midiRouteInput.midiRouteClock.propegateInputs.splice(i, 1);
         continue;
       }
     }
     this.inPortsWithoutSelfClockPropegate = this._inPortsWithoutSelfClockPropegate();
-  }  
+  }
 
-  addClockPropegateInput(){
-    if (this.propegateInputToAdd === ""){return;}
-    this.midiRouteInput.addClockPropegateInput(parseInt(this.propegateInputToAdd));
+  addClockPropegateInput() {
+    if (this.propegateInputToAdd === "") {
+      return;
+    }
+    this.midiRouteInput.addClockPropegateInput(
+      parseInt(this.propegateInputToAdd)
+    );
     this.inPortsWithoutSelfClockPropegate = this._inPortsWithoutSelfClockPropegate();
   }
 
-  _inPortsWithoutSelfClockPropegate(){
-    const keysSelected = this.midiRouteInput.midiRouteClock.propegateInputs.map( (row)=>{return row.midiInputId});    
-    return this.midiRouteInput.inPortsWithoutSelf.filter( (row)=>{return keysSelected.indexOf(row.midiInputId) === -1})
+  _inPortsWithoutSelfClockPropegate() {
+    const keysSelected = this.midiRouteInput.midiRouteClock.propegateInputs.map(
+      row => {
+        return row.midiInputId;
+      }
+    );
+    return this.midiRouteInput.inPortsWithoutSelf.filter(row => {
+      return keysSelected.indexOf(row.midiInputId) === -1;
+    });
   }
 
-
-
-  addChain(){
-    this.midiRouteInput.addMidiRouterChain("New chain" );
+  addChain() {
+    this.midiRouteInput.addMidiRouterChain("New chain");
     this.$forceUpdate();
   }
 
-get midiRouteInput() {    
-    return Connection.loginStatus.userDataConfig.getMidiRouteInput(parseInt(this.midiId))
+  get midiRouteInput() {
+    return Connection.loginStatus.userDataConfig.getMidiRouteInput(
+      parseInt(this.midiId)
+    );
   }
-
-
 
   get headrMsg(): string {
     return `${this.midiId} - ${
--- src/components/midiin/MidiInFilterChainComponent.vue
+++ src/components/midiin/MidiInFilterChainComponent.vue
@@ -1,148 +1,167 @@
 <template>
-    <div>
-        <header class="w3-container w3-blue">
-          <h5>{{chainIdx}} - <input class="w3-blue" type="text" v-model="chainItem.name">
-          <a href="#" class="router-link-exact-active router-link-active w3-right" 
-              v-if="midiRouteInput.midiRouterChains.length > 1" 
-              @click="doSplice(chainIdx)"> Delete</a>
-          </h5>
-          
-        </header>
-            
+  <div>
+    <CardHeader>
+      {{chainIdx}} -
+      <input class="w3-blue" type="text" v-model="chainItem.name" />
+      <RowCellRight v-if="midiRouteInput.midiRouterChains.length > 1">
+        <BtnHref @click="$emit('do-splice', chainIdx)">Delete</BtnHref>
+      </RowCellRight>
+    </CardHeader>
 
-        <div class="w3-container">
-          <p  v-for="(itemFilter, indexFilter) in midiRouteInput.midiRouterChains[chainIdx].midiRoutersFilters" v-bind:key="indexFilter">
-            <input class="w3-radio" type="radio"  v-model="filterSelected" :value="indexFilter" />
-            <label>{{indexFilter}} {{itemFilter.name}}</label>
-          </p>
+    <CardBody>
+      <p
+        v-for="(itemFilter, indexFilter) in midiRouteInput.midiRouterChains[chainIdx].midiRoutersFilters"
+        v-bind:key="indexFilter"
+      >
+        <input class="w3-radio" type="radio" v-model="filterSelected" :value="indexFilter" />
+        <label>{{indexFilter}} {{itemFilter.name}}</label>
+      </p>
 
-
-
-
-          <p>
-            <button class="w3-button w3-section w3-teal w3-ripple" 
-            @click="addChainFilter()">Add</button>
-            <button
-              class="w3-button w3-section w3-teal w3-ripple w3-margin-left"
-             @click="editChainFilter()">Edit</button>
-            <button
-              class="w3-button w3-section w3-teal w3-ripple w3-margin-left"
-                @click="deleteChainFilter()"              
-            >Delete</button>
-
-            <button
-              class="w3-button w3-section w3-teal w3-ripple w3-margin-left"
-              @click="doMoveUp()"
-            >
-              <i class="fa fa-arrow-up"></i>
-            </button>
-            <button
-              class="w3-button w3-section w3-teal w3-ripple w3-margin-left"
-              @click="doMoveDown()"
-            >
-              <i class="fa fa-arrow-down"></i>
-            </button>
-          </p>
-        </div>
-
-    </div>
+      <p>
+        <Btn @click="addChainFilter()">Add</Btn>
+        <Btn :ml="true" @click="editChainFilter()">Edit</Btn>
+        <Btn :ml="true" @click="deleteChainFilter()">Delete</Btn>
+        <Btn :ml="true" @click="doMoveUp()">
+          <i class="fa fa-arrow-up"></i>
+        </Btn>
+        <Btn :ml="true" @click="doMoveDown()">
+          <i class="fa fa-arrow-down"></i>
+        </Btn>
+      </p>
+    </CardBody>
+  </div>
 </template>
 
 
 <script lang="ts">
-import {MidiRouterChain} from "../../src/UserDataConfig/MidiRoutePreset/MidiRouterChain"
+import { MidiRouterChain } from "../../src/UserDataConfig/MidiRoutePreset/MidiRouterChain";
 
-import * as MRF from '../../src/UserDataConfig/MidiRoutePreset/MidiRoutersFilter'
+import * as MRF from "../../src/UserDataConfig/MidiRoutePreset/MidiRoutersFilter";
 
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { mapState } from 'vuex';
-import { mapGetters } from 'vuex'
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 
+import * as Connection from "../../src/connection";
+import * as Utils from "../../src/Utils";
 
-
-import * as Connection from '../../src/connection';
-
-
-import * as Utils from "../../src/Utils"
+import CardHeader from "../a/CardHeader.vue";
+import CardBody from "../a/CardBody.vue";
+import Btn from "../a/Btn.vue";
+import BtnHref from "../a/BtnHref.vue";
+import RowCellRight from "../a/RowCellRight.vue";
 
 @Component({
   computed: {
-    ...mapState([
-      'loginStatus'
-    ]),
-    ...mapGetters([
-      'isLoggedIn',
-    ]),
+    ...mapState(["loginStatus"]),
+    ...mapGetters(["isLoggedIn"])
   },
+  components: {
+    CardHeader,
+    CardBody,
+    RowCellRight,
+    Btn,
+    BtnHref
+  }
 })
 export default class MidiInFilterChain extends Vue {
-    @Prop() private chainItem!: MidiRouterChain;
-    @Prop() private chainIdx!: number;
-    @Prop() private midiId!: string;
-    filterSelected = ""
+  @Prop() private chainItem!: MidiRouterChain;
+  @Prop() private chainIdx!: number;
+  @Prop() private midiId!: string;
+  filterSelected = "";
 
-
-    mounted(){
-      this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters
-      
-    }
-
-    get midiRouteInput() {    
-        return Connection.loginStatus.userDataConfig.getMidiRouteInput(parseInt(this.midiId))
-    }
-
-
-    doSplice(chainIdx: number){
-      this.midiRouteInput.midiRouterChains.splice(chainIdx,1)
-    }
-
-    doMoveUp(){
-        if (this.filterSelected === ""){return;}
-        const iFilterSelected = parseInt( this.filterSelected )
-        Utils.arrayMove( this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters, iFilterSelected, iFilterSelected -1 )
-        if (iFilterSelected -1 > -1) {this.filterSelected = (iFilterSelected -1).toString()}
-        this.$forceUpdate();
-    }
-
-    doMoveDown(){
-        if (this.filterSelected === ""){return;}
-        const iFilterSelected = parseInt( this.filterSelected )
-        Utils.arrayMove( this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters, iFilterSelected, iFilterSelected + 1 )
-        if (iFilterSelected +1 < this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters.length) {this.filterSelected = (iFilterSelected +1).toString()}
-        this.$forceUpdate();
-    }
-
-    deleteChainFilter(){
-        if (this.filterSelected === ""){return;}
-        this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters.splice(parseInt(this.filterSelected),1)
-        this.$forceUpdate();
-    }
-
-    editChainFilter(){
-        if (this.filterSelected === ""){return;}
-        const filterSelectedObj = this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters[parseInt(this.filterSelected)];
-        if (filterSelectedObj.filterType === MRF.FilterType.TO_MIDI_DESTINATION){
-            this.$router.push(`/addFilter/AddMidiDestination/${this.midiId}/${this.chainIdx}/${this.filterSelected}`)
-        }
-        if (filterSelectedObj.filterType === MRF.FilterType.TO_CONSOLE){
-          this.$router.push(`/addFilter/AddConsoleLog/${this.midiId}/${this.chainIdx}/${this.filterSelected}`)
-        }
-        if (filterSelectedObj.filterType === MRF.FilterType.TO_NETWORK){
-          this.$router.push(`/addFilter/AddNetworkDestination/${this.midiId}/${this.chainIdx}/${this.filterSelected}`)
-        }
-        if (filterSelectedObj.filterType === MRF.FilterType.SCHEDULE_TO){
-          this.$router.push(`/addFilter/AddSchedule/${this.midiId}/${this.chainIdx}/${this.filterSelected}`)
-        }
-        if (filterSelectedObj.filterType === MRF.FilterType.FILTER_AND_TRANSFORM){
-          this.$router.push(`/addFilter/AddFilterTransform/${this.midiId}/${this.chainIdx}/${this.filterSelected}`)
-        }
-    }
-
-    addChainFilter(){
-    this.$router.push(`/AddChainFilter/${this.midiId}/${this.chainIdx}`);
-
-
+  mounted() {
+    this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters;
   }
 
+  get midiRouteInput() {
+    return Connection.loginStatus.userDataConfig.getMidiRouteInput(
+      parseInt(this.midiId)
+    );
+  }
+
+  doMoveUp() {
+    if (this.filterSelected === "") {
+      return;
+    }
+    const iFilterSelected = parseInt(this.filterSelected);
+    Utils.arrayMove(
+      this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters,
+      iFilterSelected,
+      iFilterSelected - 1
+    );
+    if (iFilterSelected - 1 > -1) {
+      this.filterSelected = (iFilterSelected - 1).toString();
+    }
+    this.$forceUpdate();
+  }
+
+  doMoveDown() {
+    if (this.filterSelected === "") {
+      return;
+    }
+    const iFilterSelected = parseInt(this.filterSelected);
+    Utils.arrayMove(
+      this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters,
+      iFilterSelected,
+      iFilterSelected + 1
+    );
+    if (
+      iFilterSelected + 1 <
+      this.midiRouteInput.midiRouterChains[this.chainIdx].midiRoutersFilters
+        .length
+    ) {
+      this.filterSelected = (iFilterSelected + 1).toString();
+    }
+    this.$forceUpdate();
+  }
+
+  deleteChainFilter() {
+    if (this.filterSelected === "") {
+      return;
+    }
+    this.midiRouteInput.midiRouterChains[
+      this.chainIdx
+    ].midiRoutersFilters.splice(parseInt(this.filterSelected), 1);
+    this.$forceUpdate();
+  }
+
+  editChainFilter() {
+    if (this.filterSelected === "") {
+      return;
+    }
+    const filterSelectedObj = this.midiRouteInput.midiRouterChains[
+      this.chainIdx
+    ].midiRoutersFilters[parseInt(this.filterSelected)];
+    if (filterSelectedObj.filterType === MRF.FilterType.TO_MIDI_DESTINATION) {
+      this.$router.push(
+        `/addFilter/AddMidiDestination/${this.midiId}/${this.chainIdx}/${this.filterSelected}`
+      );
+    }
+    if (filterSelectedObj.filterType === MRF.FilterType.TO_CONSOLE) {
+      this.$router.push(
+        `/addFilter/AddConsoleLog/${this.midiId}/${this.chainIdx}/${this.filterSelected}`
+      );
+    }
+    if (filterSelectedObj.filterType === MRF.FilterType.TO_NETWORK) {
+      this.$router.push(
+        `/addFilter/AddNetworkDestination/${this.midiId}/${this.chainIdx}/${this.filterSelected}`
+      );
+    }
+    if (filterSelectedObj.filterType === MRF.FilterType.SCHEDULE_TO) {
+      this.$router.push(
+        `/addFilter/AddSchedule/${this.midiId}/${this.chainIdx}/${this.filterSelected}`
+      );
+    }
+    if (filterSelectedObj.filterType === MRF.FilterType.FILTER_AND_TRANSFORM) {
+      this.$router.push(
+        `/addFilter/AddFilterTransform/${this.midiId}/${this.chainIdx}/${this.filterSelected}`
+      );
+    }
+  }
+
+  addChainFilter() {
+    this.$router.push(`/AddChainFilter/${this.midiId}/${this.chainIdx}`);
+  }
 }
 </script>
